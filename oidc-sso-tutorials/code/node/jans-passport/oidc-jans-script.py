0028a7a54937dcaa33e693f275d35b2cc5c5e91a
kdhttps <kdhttps@gmail.com>
2022-07-14 19:02:09 +0530

    feat: add oidc script

---
+++ oidc-sso-tutorials/code/node/jans-passport/oidc-jans-script.py
@@ -0,0 +1,136 @@
+# Janssen Project software is available under the Apache 2.0 License (2004). See http://www.apache.org/licenses/ for full text.
+# Copyright (c) 2020, Janssen Project
+#
+# Inbound OAuth and OpenID Connect authentication Passport Script for Janssen
+#
+# Author: Kiran Mali
+# 
+from io.jans.as.common.model.common import User
+from io.jans.as.model.common import WebKeyStorage
+from io.jans.as.model.configuration import AppConfiguration
+from io.jans.as.model.crypto import CryptoProviderFactory
+from io.jans.as.model.jwt import Jwt, JwtClaimName
+from io.jans.as.model.util import Base64Util
+from io.jans.as.server.service import AppInitializer, AuthenticationService
+from io.jans.as.common.service.common import UserService, EncryptionService
+from io.jans.as.server.service.net import HttpService
+from io.jans.as.server.security import Identity
+from io.jans.as.server.util import ServerUtil
+from io.jans.model.custom.script.type.auth import PersonAuthenticationType
+from io.jans.service.cdi.util import CdiUtil
+from io.jans.util import StringHelper
+
+from io.jans.jsf2.service import FacesService
+from jakarta.faces.application import FacesMessage
+from jakarta.faces.context import FacesContext
+from java.util import ArrayList, Arrays, Collections, UUID
+
+import json
+import sys
+import datetime
+
+class PersonAuthentication(PersonAuthenticationType):
+    def __init__(self, currentTimeMillis):
+        self.currentTimeMillis = currentTimeMillis
+
+    def init(self, customScript, configurationAttributes):
+        print "OIDC: Initialization"
+
+        # read config from oidc_creds_file
+        oidc_creds_file = configurationAttributes.get("oidc_creds_file").getValue2()
+        f = open(oidc_creds_file, 'r')
+        try:
+            creds = json.loads(f.read())
+            print creds
+        except:
+            print "OIDC: Initialization. Failed to load creds from file:", oidc_creds_file
+            print "Exception: ", sys.exc_info()[1]
+            return False
+        finally:
+            f.close()
+
+        self.op_server = str(creds["op_server"])
+        self.client_id = str(creds["client_id"])
+        self.client_secret = str(creds["client_secret"])
+        self.authorization_uri = str(creds["authorization_uri"])
+        self.token_uri = str(creds["token_uri"])
+        self.userinfo_uri = str(creds["userinfo_uri"])
+        self.redirect_uri = str(creds["redirect_uri"])
+        self.scope = str(creds["scope"]) 
+
+        print "OIDC: Initialized successfully"
+        return True
+
+    def destroy(self, configurationAttributes):
+        print "OIDC: destroy called"
+        return True
+
+    def getApiVersion(self):
+        return 11
+        
+    def getAuthenticationMethodClaims(self, requestParameters):
+        return None
+        
+    def isValidAuthenticationMethod(self, usageType, configurationAttributes):
+        return True
+
+    def getAlternativeAuthenticationMethod(self, usageType, configurationAttributes):
+        return None
+
+    def authenticate(self, configurationAttributes, requestParameters, step):
+        print "OIDC: authenticate called for step %s" % str(step)
+        identity = CdiUtil.bean(Identity)
+        if step == 1:
+            print identity.getWorkingParameter("state")
+            print identity.getWorkingParameter("nonce")
+
+        return True
+
+    def prepareForStep(self, configurationAttributes, requestParameters, step):
+        print "OIDC: prepareForStep called for step %s"  % str(step)
+        if step == 1:
+            # redirect to external OIDC server
+            state = UUID.randomUUID().toString()
+            nonce = UUID.randomUUID().toString()
+
+            redirect_url_elements = [self.authorization_uri, 
+            "?response_type=code",
+            "&client_id=", self.client_id,
+            "&scope=", self.scope,
+            "&state=", state,
+            "&nonce=", nonce,
+            "&redirect_uri=", self.redirect_uri]
+            redirect_url = "".join(redirect_url_elements)
+            
+            identity = CdiUtil.bean(Identity)
+            identity.setWorkingParameter("state", state)
+            identity.setWorkingParameter("nonce", nonce)
+
+            print identity.getWorkingParameter("state")
+
+            facesService = CdiUtil.bean(FacesService)
+            facesService.redirectToExternalURL(redirect_url)
+
+        return True
+
+    def getExtraParametersForStep(self, configurationAttributes, step):
+        print "OIDC: getExtraParametersForStep called for step %s" % str(step)
+        return Arrays.asList("redirect", "auth")
+
+    def getCountAuthenticationSteps(self, configurationAttributes):
+        print "OIDC: getCountAuthenticationSteps called"
+        return 1 
+
+    def getPageForStep(self, configurationAttributes, step):
+        print "OIDC: getPageForStep called for step %s" % str(step)
+
+    def getNextStep(self, configurationAttributes, requestParameters, step):
+        print "OIDC: getNextStep called for step %s" % str(step)
+        return step
+
+    def getLogoutExternalUrl(self, configurationAttributes, requestParameters):
+        print "OIDC: Get external logout URL call"
+        return None
+
+    def logout(self, configurationAttributes, requestParameters):
+        return True
